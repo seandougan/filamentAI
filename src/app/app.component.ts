ccbf8ecbab1ffbcbd2df734a742bd86063778f83
Sean Dougan <mediauthority@gmail.com>
2019-01-12 21:36:33 -0500

    Complete: background. Beginning widget component development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'filament-AI-test-angular';
+  title = 'Filament AI';
+  administrator = 'Rob Minford';
+  author = 'Sean Dougan';
 }
